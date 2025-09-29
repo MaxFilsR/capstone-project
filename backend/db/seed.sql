a248ee4bd41bc18f8dfbf04e7b22b414f7a73516
maximilianjaramazovic <maxi.jaramazovic@gmail.com>
2025-09-29 13:20:22 -0400

    DB interface and secrets Trying to hide login credentials and connect backend to the db.

--- backend/db/seed.sql
+++ backend/db/seed.sql
@@ -1,10 +1,9 @@
 CREATE DATABASE gainzdb;
 
-\c gainzdb
-
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  first_name
-  last_name
-  email  
+CREATE TABLE IF NOT EXISTS users (
+  ID SERIAL PRIMARY KEY,
+  first_name VARCHAR(255),
+  last_name VARCHAR(255),
+  email VARCHAR(255) UNIQUE,
+  pass VARCHAR(255) -- TODO: Change later to a more secure way of storing this info please
 );
--- backend/src/db.rs
+++ backend/src/db.rs
@@ -1,3 +1,19 @@
 /*
   This file is responsible to serve as an interface for the backend to the database. 
 */
+
+//libs
+use sqlx::{Connection, ConnectOptions};
+use sqlx::postgres::{PgConnection, PgConnectOptions};
+//const DATABASE_URL: &str= "http://localhost:5432";
+
+pub async fn get_connection(){
+  let con = PgConnectionOptions::new()
+  .host("localhost")
+  .port(5432)
+  .username()
+  .await?;
+}
+
+
+
